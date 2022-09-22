509afaca4b53f9f4794aa1acadfeacaf6cb73c7f
Agustin Guardia <guardiaagustin0@gmail.com>
2022-09-22 20:30:39 -0300

    validando datos

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ class IS{
 
 }
 
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	contraseña: /^.{4,12}$/, // 4 a 12 digitos.
+}
+let clave;
 let entrada;
 let empresa1 ;
 let contenedor_inicio=document.querySelector(".contenedor_inicio");
@@ -14,6 +19,7 @@ let formulario_login = document.querySelector(".formulario_login");
 let contenedor_login = document.querySelector(".contenedor_login");
 let mvt = document.querySelector(".contenedor_mvt")
 const formulario= document.querySelector("#formulario");
+const inputs= document.querySelectorAll ("#formulario input")
 const scanMvt= document.getElementById("scanMvt");
 const fMvt=document.querySelector(".mvt")
 const usuario1 = [];
@@ -21,27 +27,75 @@ const equiposMovistar=[];
 const equiposTelecentro=[];
 const equiposDtv=[];
 const respuesta= document.getElementById("respuesta")
-
-//eventos
-document.getElementById("btn_iniciar_sesion").addEventListener("click",inicio_sesion);
-formulario.addEventListener("submit",registro);
-scanMvt.addEventListener("click",scan);
+const campos={
+    usuario:"false",
+    contraseña: "false"
+}
 
 //mis funciones
+const validarFormulario = (e) => {
+    switch (e.target.id) {
+        case "usuario":
+            validarcampo(expresiones.usuario,e.target,"usuario");
+        break;
+        case "contraseña":
+            validarcampo(expresiones.contraseña,e.target,"contraseña");
+        break;
+        case "validacion":
+            validarcontraseña();
+        break;
+    }
+}
+const validarcampo = (expresion,input,campo) =>{
+    if (expresion.test(input.value)) {
+            document.querySelector(`.form_grupo${campo}`).classList.remove("form_grupoIncorrecto-activo");
+            document.querySelector(`.grupo_${campo}`).classList.remove("form_error");
+            campos[campo]= true;
+    } else{
+            document.querySelector(`.form_grupo${campo}`).classList.add("form_grupoIncorrecto-activo");
+            document.querySelector(`.grupo_${campo}`).classList.add("form_error");
+            campos[campo]= false;
+            }
+}
+const validarcontraseña = () =>{
+    const contraseña= document.getElementById("contraseña");
+    const validacion= document.getElementById("validacion");
+
+    if(contraseña.value!== validacion.value){
+    document.querySelector(`.form_grupovalidacion`).classList.add("form_grupoIncorrecto-activo");
+    document.querySelector(`.grupo_validacion`).classList.add("form_error");
+    campos[contraseña]= false;
+}else{
+    document.querySelector(`.form_grupovalidacion`).classList.remove("form_grupoIncorrecto-activo");
+    document.querySelector(`.grupo_validacion`).classList.remove("form_error");
+    campos[contraseña]= true;
+}
+}
 
 function inicio_sesion() {
     formulario_login.style.display= "block";
     contenedor_inicio.style.opacity= "0";
 }
 
+//eventos
+document.getElementById("btn_iniciar_sesion").addEventListener("click",inicio_sesion);
+formulario.addEventListener("submit",registro);
+inputs.forEach((input)=>{
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+});
+scanMvt.addEventListener("click",scan);
+
 
 function registro(e) {
     e.preventDefault();
-    console.log("funciona");
+    if (campos.usuario && campos.contraseña) {
     usuario1.push(document.getElementById("usuario").value,document.getElementById("contraseña").value,document.getElementById("validacion").value);
+    formulario.reset();
     formulario_login.style.display= "none";
     mvt.style.display="block";
-
+    
+    }
 }
 console.log(usuario1);
 
@@ -58,6 +112,11 @@ function scan(e) {
     }
     
 }
+
+
+/* 1 */
+
+
 
 /*function registro () {
     usuario1.push(document.getElementById("usuario").value,document.getElementById("contraseña").value,document.getElementById("validacion").value);
